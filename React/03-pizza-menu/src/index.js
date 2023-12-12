6a4f4a0139856296e7054dc25c00368b8884b5c5
Atul <atulharsh273@gmail.com>
2023-12-12 19:42:06 +0530

    finished pizza-menu

--- React/03-pizza-menu/src/index.js
+++ React/03-pizza-menu/src/index.js
@@ -56,15 +56,6 @@ function App() {
     </div>
   );
 }
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato,mozarella,spinach,and ricotta cheese</p>
-    </div>
-  );
-}
 function Header() {
   const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
   return (
@@ -74,27 +65,101 @@ function Header() {
   );
 }
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length > 0 ? true : false;
   return (
     <div className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      {numPizzas ? ( // jsx fragments -> create two elements without silgle parent.<React.fragment key={}>
+        <>
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
+            non quis exercitationem culpa nesciunt nihil aut nostrum explicabo
+            reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
+            Voluptatum ducimus voluptates voluptas?
+          </p>
+          <div className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaOb={pizza} key={pizza.name} />
+            ))}
+          </div>
+        </>
+      ) : (
+        <p>We're still working on our menu, Please come back later :)</p>
+      )}
+
+      {/*<div className="pizzas">
+        {pizzaData.map(
+          (
+            pizza //rendering list
+          ) => (
+            <Pizza pizzaOb={pizza} key={pizza.name} />
+          )
+        )}
+          </div>*/}
+
+      {/* <Pizza
+        name="Pizza Spinaci"
+        ingredient="Tomato,mozarella,spinach,and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price="10"
+        />
+        <Pizza
+        name="Pizza Funghi"
+        ingredient="Tomato,mushrooms"
+        photoName="pizzas/funghi.jpg"
+        price="12"
+        />
+        <Pizza />
+      <Pizza /> */}
+    </div>
+  );
+}
+function Pizza(props) {
+  // we can also destructure props like -> ({props})
+  //props are only read-only,react allows data flow in one way only , parent to child components
+  console.log(props);
+  return (
+    <div className={props.pizzaOb.soldOut ? "pizza sold-out" : "pizza"}>
+      <img src={props.pizzaOb.photoName} alt={props.pizzaOb.name} />
+      <div>
+        <h3>{props.pizzaOb.name}</h3>
+        <p>{props.pizzaOb.ingredients}</p>
+        <span>
+          {props.pizzaOb.soldOut ? "Sold Out" : Number(props.pizzaOb.price) + 3}
+        </span>
+      </div>
     </div>
   );
 }
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 9;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
   // else alert("Sorry we are closed");
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}.We're currently open
+      {
+        /* conditional rendering*/
+        isOpen && <Order closeHour={closeHour} />
+        // isOpen ? <p>We're currently open</p> : <p>We're currently closed!</p>
+      }
     </footer>
   );
+  /*
+  we can also use multiple return statement in js for conditional rendering:
+  if(isopen)
+      return(
+        //jsx
+      )
+  else
+      return(
+        //jsx
+      )
+  */
   // return React.createElement(
   //   "footer",
   //   null,
@@ -102,6 +167,19 @@ function Footer() {
   // `
   // );
 }
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        we're currently open, Untill {props.closeHour}:00 come visit us or order
+        online
+      </p>
+      <button className="btn">Order Now</button>
+    </div>
+  );
+}
+
 // react v18
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -114,3 +192,10 @@ root.render(
 
 //react before 18
 // React.render(<App/>);
+
+/*
+General jsx Rules:
+jsx works like html but we can enter javascript mode using {}
+we can replace js expressions inside {}.
+a piece of jsx can only have one root element.
+*/
