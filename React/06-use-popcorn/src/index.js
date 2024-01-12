4df7732857e252bcae81434dc2012ebafd7f4800
Atul <atulharsh273@gmail.com>
2024-01-12 23:05:42 +0530

    updated

--- React/06-use-popcorn/src/index.js
+++ React/06-use-popcorn/src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={6} />
+    <StarRating maxRating={3} messages={["poor", "good", "cool!"]} />
   </React.StrictMode>
 );
 
